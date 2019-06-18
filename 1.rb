02b5d3012e471688cdbe1db9d6da09bc40836f15
Marcos Villegas <dinavers@gmail.com>
2019-06-18 19:52:02 -0400

    ejercicio 1

---
+++ 1.rb
@@ -0,0 +1,25 @@
+# Corregir los errores para poder ejecutar ambos métodos.
+
+# class MiClase
+#   def de_instancia
+#     puts 'Método de instancia!'
+#   end
+#   def.self de_clase
+#     puts 'Método de clase!'
+#   end
+# end
+#
+# MiClase.de_instancia
+# MiClase.new.de_clase
+
+class MiClase
+  def de_instancia
+    puts 'Método de instancia!'
+  end
+  def de_clase
+    puts 'Método de clase!'
+  end
+end
+
+MiClase.new.de_instancia
+MiClase.new.de_clase
